32334449cc2895d5f293c16feaef2c452291aa96
Pavlo Panko <pavlikosar@gmail.com>
2025-04-19 20:06:41 +0300

    Fix deploy action

---
+++ src/Infrastructure/Core/CoreDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Core;
+
+public class CoreDbContextFactory : IDesignTimeDbContextFactory<CoreDbContext>
+{
+	public CoreDbContext CreateDbContext(string[] args)
+	{
+		var connectionString = args.Length != 0 ? args[0] : throw new InvalidOperationException("Connection string not provided via appsettings.json or command-line arguments.");
+
+		var optionsBuilder = new DbContextOptionsBuilder<CoreDbContext>();
+		optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+			mysqlOptions => mysqlOptions
+				.EnableRetryOnFailure(
+					maxRetryCount: 3,
+					maxRetryDelay: TimeSpan.FromSeconds(5),
+					errorNumbersToAdd: null))
+			.UseSnakeCaseNamingConvention();
+
+		return new CoreDbContext(optionsBuilder.Options);
+	}
+}
