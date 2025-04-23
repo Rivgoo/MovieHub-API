758a7abb9297a80c0ebe3bb0c7503b427834dcf3
Pavlo Panko <pavlikosar@gmail.com>
2025-04-23 11:13:40 +0300

    feat: remove unnecessary data from login response

--- src/Web.API/Controllers/V1/Authentications/AuthenticationController.cs
+++ src/Web.API/Controllers/V1/Authentications/AuthenticationController.cs
@@ -91,8 +91,6 @@ public class AuthenticationController(
 
 		var response = new AuthenticationResponse()
 		{
-			UserId = authenticationResult.User!.Id,
-			UserRole = authenticationResult.User.Role,
 			AccessToken = _jwtAuthentication.GenerateToken(authenticationResult.User!.Id, authenticationResult.User.Role)
 		};
 
--- src/Web.API/Controllers/V1/Authentications/AuthenticationResponse.cs
+++ src/Web.API/Controllers/V1/Authentications/AuthenticationResponse.cs
@@ -7,16 +7,4 @@ public class AuthenticationResponse
 	/// </summary>
 	/// <value>The generated access token string if successful, or <see cref="string.Empty"/> on failure.</value>
 	public string AccessToken { get; set; } = string.Empty;
-
-	/// <summary>
-	/// Gets or sets the unique identifier of the user.
-	/// </summary>
-	/// <value>The user's unique ID.</value>
-	public string UserId { get; set; }
-
-	/// <summary>
-	/// Gets or sets the primary role of the user.
-	/// </summary>
-	/// <value>The main role assigned to the user (e.g., "Admin", "Customer").</value>
-	public string UserRole { get; set; }
 }
